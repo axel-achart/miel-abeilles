75b824f4d032a905fbf333d28f174e97fe3b20ea
hippo <hippolyte.geslain@laplateforme.io>
2025-10-03 13:01:47 +0200

    pseudo-code

--- comparison.py
+++ comparison.py
@@ -1,3 +1,9 @@
 #file for comparison of all csv's
 
 data_dir = "data"
+
+chercher dossiers de mutation rate différents
+pour chaque dossier, 
+    faire une analyse statistique
+Utiliser chaque analyse pour garder 3 variables communes
+Visualiser ces 3 variables communes
